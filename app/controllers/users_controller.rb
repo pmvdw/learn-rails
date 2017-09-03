c09e661ed7f69839497f96c24905f9abe3f1d7c4
pmvdw <pmvdw@apples-MacBook-Pro.local>
2017-09-03 11:16:29 -0400

    deleted user controller and route

--- app/controllers/users_controller.rb
+++
@@ -1,7 +0,0 @@
-class UsersController < ApplicationController
-
-  def index
-    render html: "we're in the index action of the users controller!"
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#dog'
 
-  get 'users',  to: 'users#index'
-
   get 'apple', to: 'fruits#apple'
 
   get 'pear', to: 'fruits#pear'
