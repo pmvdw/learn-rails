3468261f875dfd04cb858b0102632d6a986220d6
pmvdw <pmvdw@apples-MacBook-Pro.local>
2017-09-03 12:43:59 -0400

    added apple and pear content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "hello, world!"
-  end
-
-
   def dog
-    render html: "woof, woof!"
   end
 end
-
-
-
-# class ApplicationController < ActionController::Base
-#   protect_from_forgery with: :exception
-
-#   def hello
-#     render html: "hello, world!"
-#   end
-# end
-
--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -1,11 +1,9 @@
 class FruitsController < ApplicationController
 
   def apple
-    render html: "apple"
   end
 
   def pear
-    render html: "pear"
   end
 
 end
